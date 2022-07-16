85c0b8d163c5d3acf48deac569fd272ca216f5f1
김성섭 <chocobo200@cnspartner.com>
2022-07-16 19:07:06 +0900

    18장 - 예외처리 추가

---
+++ src/main/java/chap18/Sample01.java
@@ -0,0 +1,14 @@
+package chap18;
+
+public class Sample01 {
+    public static void main(String[] args){
+        int i = 1;;
+        try{
+            int j = 1;
+            System.out.println(i);  // 출력 됨
+        } catch (Exception e){
+
+        }
+       // System.out.println(j);  /// 오류 발생
+    }
+}
